1ea9dfccca46a2a6ad8efa80cc0b57e8c6d0ec38
Walesfarm <157367288+Walesfarm@users.noreply.github.com>
2024-01-31 23:32:46 +0600

    Add files via upload

---
+++ TSIS 2/Lists.py
@@ -0,0 +1,22 @@
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = "kiwi"
+
+a = fruits.append("orange")
+
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1,"lemon")
+
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
